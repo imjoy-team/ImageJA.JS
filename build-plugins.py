9c6d8077cc170be9ba96657eb5a901ab7582e9db
Wei Ouyang <oeway007@gmail.com>
2020-09-03 02:01:56 +0200

    Fix build plugins

--- build-plugins.py
+++ build-plugins.py
@@ -12,29 +12,49 @@ Created-By: 1.8.0_172-b11 (Oracle Corporation)
 
 CHEERPJ_DIR = os.environ.get("CHEERPJ_DIR")
 
-with open(plugins_dir+'/index.list', 'w') as index: 
-    for root, dirs, files in os.walk(plugins_dir, topdown=False):
-        for name in files:
-            filename = os.path.join(root, name)
-            if CHEERPJ_DIR and name.endswith('.class'):
-                print('adding '+ filename)
-                with ZipFile(filename.replace('.class', '.jar'), 'w') as zip:
-                    zip.writestr('META-INF/MANIFEST.MF', MANIFEST)
-                    zip.write(filename, arcname=name)
-                # escape $ sign
-                filename = filename.replace('$', '\$')
-                os.system(CHEERPJ_DIR + '/cheerpjfy.py --deps=' +ij_path+ " "+ filename.replace('.class', '.jar'))
-                os.system('rm ' + filename.replace('.class', '.jar'))
-                os.system('mv ' + filename.replace('.class', '.jar.js') + ' ' + filename.replace('.class', '.js'))
-                if os.path.exists(filename.replace('.class', '.jar.js')):
-                    index.write(os.path.relpath(filename, plugins_dir) + '\n')
-            elif os.path.relpath(filename, plugins_dir).startswith('jars') and name.endswith('.jar'):
-                # with ZipFile(jar_file_name, 'w') as zip:
-                #     zip.writestr('META-INF/MANIFEST.MF', MANIFEST)
-                #     zip.write(filename, arcname=name)
-                pass
-            elif name.endswith('.jar') and os.path.exists(filename+'.js'):
-                index.write(os.path.relpath(filename, plugins_dir))
-            else:
-                print('skipping '+ filename)
-    
+print('====> Building plugin list...')
+for root, dirs, files in os.walk(plugins_dir, topdown=False):
+    for name in files:
+        filename = os.path.join(root, name)
+        
+        if CHEERPJ_DIR and name.endswith('.class'):
+            print("====> Compiling " + filename)
+            with ZipFile(filename.replace('.class', '.jar'), 'w') as zip:
+                zip.writestr('META-INF/MANIFEST.MF', MANIFEST)
+                zip.write(filename, arcname=name)
+            # escape $ sign
+            filename = filename.replace('$', '\$')
+            os.system(CHEERPJ_DIR + '/cheerpjfy.py --deps=' +ij_path+ " "+ filename.replace('.class', '.jar'))
+            os.system('rm ' + filename.replace('.class', '.jar'))
+            os.system('mv ' + filename.replace('.class', '.jar.js') + ' ' + filename.replace('.class', '.js'))
+        elif os.path.relpath(filename, plugins_dir).startswith('jars') and name.endswith('.jar'):
+            # with ZipFile(jar_file_name, 'w') as zip:
+            #     zip.writestr('META-INF/MANIFEST.MF', MANIFEST)
+            #     zip.write(filename, arcname=name)
+            print("====> Compiling " + filename)
+            os.system(CHEERPJ_DIR + '/cheerpjfy.py '+ filename)
+        else:
+            print('====> Skipping '+ filename)
+
+# building index.list
+def index_dir(cdir):
+    with open(os.path.join(cdir, 'index.list'), 'w') as index:
+        for file_name in os.listdir(cdir):
+            fname = os.path.splitext(file_name)[0]
+            if os.path.isfile(os.path.join(cdir, file_name)):
+                if file_name.endswith('.class') and os.path.exists(os.path.join(cdir, fname+".js")):
+                    print('Adding ' + os.path.join(cdir, file_name))
+                    index.write(file_name+"\n")
+                if file_name.endswith('.jar')  and os.path.exists(os.path.join(cdir, fname+".jar.js")):
+                    print('Adding ' + os.path.join(cdir, file_name))
+                    index.write(file_name+"\n")
+            elif os.path.isdir(os.path.join(cdir, file_name)):
+                index.write(file_name+"\n")
+            
+print('====> Building index.list for ' + os.path.abspath(plugins_dir))
+index_dir(plugins_dir)
+for root, dirs, files in os.walk(plugins_dir, topdown=False):
+    for d in dirs:
+        index_dir(os.path.join(root, d))
+
+print('Done')
--- compile-js-dev.sh
+++ compile-js-dev.sh
@@ -3,6 +3,8 @@
 
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 mvn install:install-file -Dfile=${CHEERPJ_DIR}/cheerpj-dom.jar -DgroupId=com.learningtech -DartifactId=cheerpj-dom -Dversion=1.0 -Dpackaging=jar
 mvn -Pdeps package
 cp target/ij-1.53c.jar ${IJ_DIR}/ij.jar
@@ -13,3 +15,5 @@ ${CHEERPJ_DIR}/cheerpjfy.py ij.jar
 # curl https://github.com/zitmen/thunderstorm/releases/download/v1.3/Thunder_STORM.jar -LO
 # mv Thunder_STORM.jar plugins/Thunder_STORM.jar
 # ${CHEERPJ_DIR}/cheerpjfy.py  --deps=ij.jar plugins/Thunder_STORM.jar
+
+# python ${DIR}/build-plugins.py
