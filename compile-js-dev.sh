795a64d7c2180897159398bde70358b1ace99fcc
Wei Ouyang <oeway007@gmail.com>
2020-09-21 17:14:14 +0200

    upgrade to 1.53e

--- compile-js-dev.sh
+++ compile-js-dev.sh
@@ -3,18 +3,18 @@
 
 set -e
 
-IJ_JAR="ij-1.53d.jar"
+IJ_JAR="ij-1.53e.jar"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mvn install:install-file -Dfile=${CHEERPJ_DIR}/cheerpj-dom.jar -DgroupId=com.learningtech -DartifactId=cheerpj-dom -Dversion=1.0 -Dpackaging=jar
 mvn -Pdeps package
 cp target/${IJ_JAR} ${IJ_DIR}/
 
-cp ${CHEERPJ_DIR}/cheerpj-dom.jar ${IJ_DIR}/cheerpj-dom-1.0.jar
-${CHEERPJ_DIR}/cheerpjfy.py ${IJ_DIR}/cheerpj-dom-1.0.jar
-
 cd ${IJ_DIR}
 ${CHEERPJ_DIR}/cheerpjfy.py ${IJ_JAR}
+
+cp ${CHEERPJ_DIR}/cheerpj-dom.jar ${IJ_DIR}/cheerpj-dom-1.0.jar
+${CHEERPJ_DIR}/cheerpjfy.py ${IJ_DIR}/cheerpj-dom-1.0.jar
 
 curl https://github.com/zitmen/thunderstorm/releases/download/v1.3/Thunder_STORM.jar -LO
 mv Thunder_STORM.jar plugins/Thunder_STORM.jar
